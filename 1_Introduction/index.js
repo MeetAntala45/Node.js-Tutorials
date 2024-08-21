a162d5216a65dfe38569c67d85aff49c3d0c2738
Meet <meetantala45@gmail.com>
2024-08-21 19:38:19 +0530

    Introduction

---
+++ 1_Introduction/index.js
@@ -0,0 +1,5 @@
+// Node.js is javascript runtime built on Chrome's V8 javascript engine
+// Node.js is a javaScript running on the server
+
+// used in data streaming application
+// used in real time chat application
