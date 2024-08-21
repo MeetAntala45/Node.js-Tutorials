f7580a8677f8501f18e11411c21713bd934d34e0
Meet <meetantala45@gmail.com>
2024-08-21 23:40:24 +0530

    OS Module

---
+++ 10_os_module/index.js
@@ -0,0 +1,26 @@
+const os = require("os");
+
+// to show 32 bit or 64 bit architecture  // x64
+console.log(os.arch());
+
+// to show free memory
+const freeMemory = os.freemem();
+console.log(freeMemory);
+console.log(`${freeMemory/1024/1024/1024}`);
+
+// to show total memory
+const totalMemory = os.totalmem();
+console.log(totalMemory);
+console.log(`${totalMemory/1024/1024/1024}`);
+
+// to show hostname
+console.log(os.hostname());
+
+//to show platform
+console.log(os.platform());
+
+// to show temporary
+console.log(os.tmpdir());
+
+// to show operating system type
+console.log(os.type());
