1e0b6fa13c9efc8891338f46ceba2853a8c4690a
Meet <meetantala45@gmail.com>
2024-08-21 18:45:18 +0530

    Completed Video 4

---
+++ 4_REPL/index.js
@@ -0,0 +1,7 @@
+// type node in cmd
+
+// 1. JS Expression
+// 2. use variables
+// 3. Multiline code / loops
+// 4. use (_) to get the last result
+// 5. We can use editor mode by run .editor command
